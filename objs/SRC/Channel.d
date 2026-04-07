76804727e3c74b5b80553d753af0c6a6ffeb057d
oshcheho <oshcheho@student.42vienna.com>
2026-04-07 16:17:18 +0200

    deleted objs and deps

--- objs/SRC/Channel.d
+++
@@ -1,6 +0,0 @@
-objs/SRC/Channel.o: SRC/Channel.cpp SRC/../INC/Channel.hpp \
-  SRC/../INC/Client.hpp
-
-SRC/../INC/Channel.hpp:
-
-SRC/../INC/Client.hpp:
--- objs/SRC/Client.d
+++
@@ -1,3 +0,0 @@
-objs/SRC/Client.o: SRC/Client.cpp SRC/../INC/Client.hpp
-
-SRC/../INC/Client.hpp:
